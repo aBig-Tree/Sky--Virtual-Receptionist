95e8e9183d6236a6b2fc228b0466573d3e541901
Steve <39825266+aBig-Tree@users.noreply.github.com>
2019-01-22 11:00:55 +0000

    Update reception-staff-app.php
    Improved the boxes so that additional users can be added easily and tidied up the CSS.  Now, you can add as many boxes as you want and they won't show up until you assign a user to them.

--- reception-staff-app.php
+++ reception-staff-app.php
@@ -12,20 +12,23 @@
 body {background-color: #033047; font-family: Arial, Helvetica, sans-serif; }
 iframe {display: none;}
 form {display: inline-block; width: 49%;}
-
 .clocking-container {background: #000;}
 .clocking-container h1 {font-size: 18pt; text-align: center; padding: 10px 0; margin: 0; color: #fff;}
 .clocking-options {width: 100%; display: inline-block;}
-
 input[type=submit] {height: 100%; color: #fff; font-size:17pt; white-space: normal; padding: 8px 5px; margin: 0; border: none;}
 input.clocking-box.clocking-in[type=submit] {width: 100%; background: #93c447;}
 input.clocking-box.clocking-out[type=submit] {width: 100%; background: #f12c2c;}
-
 textarea, input.text, input[type="text"], input[type="button"], input[type="submit"], .input-checkbox {-webkit-appearance: none; border-radius: 0;}/* Get Off My Lawn!!! - tell iOS to behave */
 .features-block {display: inline-block; width: 100%;}
 .features-block-box {width: auto; min-width: 230px; padding: 10px; float: left;}
-
-
+.ConfirmationHide {opacity: 0;}
+.ConfirmationShow {-o-transition: opacity 1s; -moz-transition: opacity 1s; -webkit-transition: opacity 1s; transition: opacity 1s; opacity:1;}
+#ConfirmationMessageIN {background: #93c447; font-size: 30pt; color: #fff; display: block; position: fixed; bottom: 0; left: 0; width: 100%; z-index: 99; padding: 30px 0; text-align: center; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);}
+#ConfirmationMessageOUT {background: #f12c2c; font-size: 30pt; color: #fff; display: block; position: fixed; bottom: 0; left: 0; width: 100%; z-index: 99; padding: 30px 0; text-align: center; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);}
+#time {color: #fff; font-size: 24pt; text-align: center; right: 0; bottom: 0; display: block; position: absolute; padding: 10px;}
+.clocking-container h1:empty {display: none;}/*hide empty clocking in options*/
+.clocking-container h1:empty + .clocking-options {display: none;}/*hide empty clocking in options*/
+.clocking-container h1:empty + input {display: none;}/*hide empty clocking in options*/
 @media screen and (max-width: 600px) {
     .features-block {display: block; max-width: 100%;}
     .features-block-box {display: inline-block; width: 100%; float: none; padding: 0;}   
@@ -52,12 +55,22 @@ textarea, input.text, input[type="text"], input[type="button"], input[type="subm
 <!-- When conversing with her, you'll find she only responds to 'Sky'.  Her favourite food is a Cheese Toasty. -->
 <!-- DO NOT DELETE -->
 <iframe width="0" height="0" border="0" name="dummyframe" id="dummyframe"></iframe>
+	
+<!-- EDIT this section and add the users to match the one in reception-app.php.  If you add more than 10 users, you'll need to add additional code below by copying and pasting the code and sequentially increasing the user count.  You'll figure it out... -->
 <?php  
 date_default_timezone_set('Europe/London');
-$ZapierURL = "YOUR_ZAPIER_URL_HERE?employee=";
-$user1 = "Joe Blogs";
-$user2 = "Someone Else";
-$user3 = "Another Person";
+	$ZapierURL = "YOUR_ZAPIER_URL_HERE?employee=";
+	$HostingLocation = "YOUR DOMAIN HERE";
+	$user1 = "Joe Blogs";
+	$user2 = "Someone Else";
+	$user3 = "Another Person";
+	$user4 = "";
+	$user5 = "";
+	$user6 = "";
+	$user7 = "";
+	$user8 = "";
+	$user9 = "";
+	$user10 = "";	
 ?>
 <!-- DO NOT DELETE -->
 
@@ -67,10 +80,10 @@ $user3 = "Another Person";
 		<div class="clocking-container">
 			<h1><?php echo $user1 ?></h1>
 			<div class="clocking-options">
-			<form action="<?php echo $ZapierURL ?><?php echo $user1 ?>&direction=IN&date=<?php echo date('d-m-Y')?>&time=<?php echo date('h:i:sa')?>" method="post" target="dummyframe">
+			<form action="<?php echo $ZapierURL ?><?php echo $user1 ?>&direction=IN&date=<?php echo date('d-m-Y')?>&time=<?php echo date('h:i:s A')?>&time_date=<?php echo date('d-m-Y h:i:s A')?>" method="post" target="dummyframe">
 		  		<input class="clocking-box clocking-in" type="submit" onclick="clickEventIN()" value="IN">
 			</form> 
-			<form action="<?php echo $ZapierURL ?><?php echo $user1 ?>&direction=OUT&date=<?php echo date('d-m-Y')?>&time=<?php echo date('h:i:sa')?>" method="post" target="dummyframe">
+			<form action="<?php echo $ZapierURL ?><?php echo $user1 ?>&direction=OUT&date=<?php echo date('d-m-Y')?>&time=<?php echo date('h:i:s A')?>&time_date=<?php echo date('d-m-Y h:i:s A')?>" method="post" target="dummyframe">
 		  		<input class="clocking-box clocking-out" type="submit" onclick="clickEventOUT()" value="OUT">
 			</form> 
 			</div>
@@ -81,10 +94,10 @@ $user3 = "Another Person";
 		<div class="clocking-container">
 			<h1><?php echo $user2 ?></h1>
 			<div class="clocking-options">
-			<form action="<?php echo $ZapierURL ?><?php echo $user2 ?>&direction=IN&date=<?php echo date('d-m-Y')?>&time=<?php echo date('h:i:sa')?>" method="post" target="dummyframe">
+			<form action="<?php echo $ZapierURL ?><?php echo $user2 ?>&direction=IN&date=<?php echo date('d-m-Y')?>&time=<?php echo date('h:i:s A')?>&time_date=<?php echo date('d-m-Y h:i:s A')?>" method="post" target="dummyframe">
 		  		<input class="clocking-box clocking-in" type="submit" onclick="clickEventIN()" value="IN">
 			</form> 
-			<form action="<?php echo $ZapierURL ?><?php echo $user2 ?>&direction=OUT&date=<?php echo date('d-m-Y')?>&time=<?php echo date('h:i:sa')?>" method="post" target="dummyframe">
+			<form action="<?php echo $ZapierURL ?><?php echo $user2 ?>&direction=OUT&date=<?php echo date('d-m-Y')?>&time=<?php echo date('h:i:s A')?>&time_date=<?php echo date('d-m-Y h:i:s A')?>" method="post" target="dummyframe">
 		  		<input class="clocking-box clocking-out" type="submit" onclick="clickEventOUT()" value="OUT">
 			</form> 
 			</div>
@@ -95,17 +108,115 @@ $user3 = "Another Person";
 		<div class="clocking-container">
 			<h1><?php echo $user3 ?></h1>
 			<div class="clocking-options">
-			<form action="<?php echo $ZapierURL ?><?php echo $user3 ?>&direction=IN&date=<?php echo date('d-m-Y')?>&time=<?php echo date('h:i:sa')?>" method="post" target="dummyframe">
+			<form action="<?php echo $ZapierURL ?><?php echo $user3 ?>&direction=IN&date=<?php echo date('d-m-Y')?>&time=<?php echo date('h:i:s A')?>&time_date=<?php echo date('d-m-Y h:i:s A')?>" method="post" target="dummyframe">
 		  		<input class="clocking-box clocking-in" type="submit" onclick="clickEventIN()" value="IN">
 			</form> 
-			<form action="<?php echo $ZapierURL ?><?php echo $user3 ?>&direction=OUT&date=<?php echo date('d-m-Y')?>&time=<?php echo date('h:i:sa')?>" method="post" target="dummyframe">
+			<form action="<?php echo $ZapierURL ?><?php echo $user3 ?>&direction=OUT&date=<?php echo date('d-m-Y')?>&time=<?php echo date('h:i:s A')?>&time_date=<?php echo date('d-m-Y h:i:s A')?>" method="post" target="dummyframe">
 		  		<input class="clocking-box clocking-out" type="submit" onclick="clickEventOUT()" value="OUT">
 			</form> 
 			</div>
 		</div>
 	</div>
 
-<!-- copy and paste one of the 'features-block-box in-out' DIVs above this line and update the user number accordingly -->
+	<div class="features-block-box in-out">
+		<div class="clocking-container">
+			<h1><?php echo $user4 ?></h1>
+			<div class="clocking-options">
+			<form action="<?php echo $ZapierURL ?><?php echo $user4 ?>&direction=IN&date=<?php echo date('d-m-Y')?>&time=<?php echo date('h:i:s A')?>&time_date=<?php echo date('d-m-Y h:i:s A')?>" method="post" target="dummyframe">
+		  		<input class="clocking-box clocking-in" type="submit" onclick="clickEventIN()" value="IN">
+			</form> 
+			<form action="<?php echo $ZapierURL ?><?php echo $user4 ?>&direction=OUT&date=<?php echo date('d-m-Y')?>&time=<?php echo date('h:i:s A')?>&time_date=<?php echo date('d-m-Y h:i:s A')?>" method="post" target="dummyframe">
+		  		<input class="clocking-box clocking-out" type="submit" onclick="clickEvent()" value="OUT">
+			</form> 
+			</div>
+		</div>
+	</div>
+
+	<div class="features-block-box in-out">
+		<div class="clocking-container">
+			<h1><?php echo $user5 ?></h1>
+			<div class="clocking-options">
+			<form action="<?php echo $ZapierURL ?><?php echo $user5 ?>&direction=IN&date=<?php echo date('d-m-Y')?>&time=<?php echo date('h:i:s A')?>&time_date=<?php echo date('d-m-Y h:i:s A')?>" method="post" target="dummyframe">
+		  		<input class="clocking-box clocking-in" type="submit" onclick="clickEventIN()" value="IN">
+			</form> 
+			<form action="<?php echo $ZapierURL ?><?php echo $user5 ?>&direction=OUT&date=<?php echo date('d-m-Y')?>&time=<?php echo date('h:i:s A')?>&time_date=<?php echo date('d-m-Y h:i:s A')?>" method="post" target="dummyframe">
+		  		<input class="clocking-box clocking-out" type="submit" onclick="clickEventOUT()" value="OUT">
+			</form> 
+			</div>
+		</div>
+	</div>
+
+	<div class="features-block-box in-out">
+		<div class="clocking-container">
+			<h1><?php echo $user6 ?></h1>
+			<div class="clocking-options">
+			<form action="<?php echo $ZapierURL ?><?php echo $user6 ?>&direction=IN&date=<?php echo date('d-m-Y')?>&time=<?php echo date('h:i:s A')?>&time_date=<?php echo date('d-m-Y h:i:s A')?>" method="post" target="dummyframe">
+		  		<input class="clocking-box clocking-in" type="submit" onclick="clickEventIN()" value="IN">
+			</form> 
+			<form action="<?php echo $ZapierURL ?><?php echo $user6 ?>&direction=OUT&date=<?php echo date('d-m-Y')?>&time=<?php echo date('h:i:s A')?>&time_date=<?php echo date('d-m-Y h:i:s A')?>" method="post" target="dummyframe">
+		  		<input class="clocking-box clocking-out" type="submit" onclick="clickEventOUT()" value="OUT">
+			</form> 
+			</div>
+		</div>
+	</div>
+
+	<div class="features-block-box in-out">
+		<div class="clocking-container">
+			<h1><?php echo $user7 ?></h1>
+			<div class="clocking-options">
+			<form action="<?php echo $ZapierURL ?><?php echo $user7 ?>&direction=IN&date=<?php echo date('d-m-Y')?>&time=<?php echo date('h:i:s A')?>&time_date=<?php echo date('d-m-Y h:i:s A')?>" method="post" target="dummyframe">
+		  		<input class="clocking-box clocking-in" type="submit" onclick="clickEventIN()" value="IN">
+			</form> 
+			<form action="<?php echo $ZapierURL ?><?php echo $user7 ?>&direction=OUT&date=<?php echo date('d-m-Y')?>&time=<?php echo date('h:i:s A')?>&time_date=<?php echo date('d-m-Y h:i:s A')?>" method="post" target="dummyframe">
+		  		<input class="clocking-box clocking-out" type="submit" onclick="clickEventOUT()" value="OUT">
+			</form> 
+			</div>
+		</div>
+	</div>
+
+	<div class="features-block-box in-out">
+		<div class="clocking-container">
+			<h1><?php echo $user8 ?></h1>
+			<div class="clocking-options">
+			<form action="<?php echo $ZapierURL ?><?php echo $user8 ?>&direction=IN&date=<?php echo date('d-m-Y')?>&time=<?php echo date('h:i:s A')?>&time_date=<?php echo date('d-m-Y h:i:s A')?>" method="post" target="dummyframe">
+		  		<input class="clocking-box clocking-in" type="submit" onclick="clickEventIN()" value="IN">
+			</form> 
+			<form action="<?php echo $ZapierURL ?><?php echo $user8 ?>&direction=OUT&date=<?php echo date('d-m-Y')?>&time=<?php echo date('h:i:s A')?>&time_date=<?php echo date('d-m-Y h:i:s A')?>" method="post" target="dummyframe">
+		  		<input class="clocking-box clocking-out" type="submit" onclick="clickEventOUT()" value="OUT">
+			</form> 
+			</div>
+		</div>
+	</div>
+
+	<div class="features-block-box in-out">
+		<div class="clocking-container">
+			<h1><?php echo $user9 ?></h1>
+			<div class="clocking-options">
+			<form action="<?php echo $ZapierURL ?><?php echo $user9 ?>&direction=IN&date=<?php echo date('d-m-Y')?>&time=<?php echo date('h:i:s A')?>&time_date=<?php echo date('d-m-Y h:i:s A')?>" method="post" target="dummyframe">
+		  		<input class="clocking-box clocking-in" type="submit" onclick="clickEventIN()" value="IN">
+			</form> 
+			<form action="<?php echo $ZapierURL ?><?php echo $user9 ?>&direction=OUT&date=<?php echo date('d-m-Y')?>&time=<?php echo date('h:i:s A')?>&time_date=<?php echo date('d-m-Y h:i:s A')?>" method="post" target="dummyframe">
+		  		<input class="clocking-box clocking-out" type="submit" onclick="clickEventOUT()" value="OUT">
+			</form> 
+			</div>
+		</div>
+	</div>
+
+	<div class="features-block-box in-out">
+		<div class="clocking-container">
+			<h1><?php echo $user10 ?></h1>
+			<div class="clocking-options">
+			<form action="<?php echo $ZapierURL ?><?php echo $user10 ?>&direction=IN&date=<?php echo date('d-m-Y')?>&time=<?php echo date('h:i:s A')?>&time_date=<?php echo date('d-m-Y h:i:s A')?>" method="post" target="dummyframe">
+		  		<input class="clocking-box clocking-in" type="submit" onclick="clickEventIN()" value="IN">
+			</form> 
+			<form action="<?php echo $ZapierURL ?><?php echo $user10 ?>&direction=OUT&date=<?php echo date('d-m-Y')?>&time=<?php echo date('h:i:s A')?>&time_date=<?php echo date('d-m-Y h:i:s A')?>" method="post" target="dummyframe">
+		  		<input class="clocking-box clocking-out" type="submit" onclick="clickEventOUT()" value="OUT">
+			</form> 
+			</div>
+		</div>
+	</div>
+
+
 
 </div>
 
@@ -115,12 +226,6 @@ $user3 = "Another Person";
 <div id="ConfirmationMessageOUT" class="ConfirmationHide">Goodbye</div>
 <!-- DO NOT DELETE -->
 
-<style>
-.ConfirmationHide {opacity: 0;}
-.ConfirmationShow {-o-transition: opacity 1s; -moz-transition: opacity 1s; -webkit-transition: opacity 1s; transition: opacity 1s; opacity:1;}
-#ConfirmationMessageIN {background: #93c447; font-size: 30pt; color: #fff; display: block; position: fixed; bottom: 0; left: 0; width: 100%; z-index: 99; padding: 30px 0; text-align: center; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);}
-#ConfirmationMessageOUT {background: #f12c2c; font-size: 30pt; color: #fff; display: block; position: fixed; bottom: 0; left: 0; width: 100%; z-index: 99; padding: 30px 0; text-align: center; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);}
-</style>
 
 <script>
 // Morning IN event
@@ -128,7 +233,7 @@ function clickEventIN() {
 	ConfirmationPopupIN();
 
     var timer = setTimeout(function() {
-        window.location='[YOUR-WEB-ADDRESS]/reception-app.php'
+        window.location='<?php echo $HostingLocation ?>/reception-app.php'
     }, 2000);
 }
 
@@ -143,7 +248,7 @@ function clickEventOUT() {
 	ConfirmationPopupOUT();
 
     var timer = setTimeout(function() {
-        window.location='[YOUR-WEB-ADDRESS]/reception-app.php'
+        window.location='<?php echo $HostingLocation ?>/reception-app.php'
     }, 2000);
 }
 
